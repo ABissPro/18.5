40664367011586b74ffdffb01eae9f5da59a05cd
ABissPro <109984434+ABissPro@users.noreply.github.com>
2025-02-25 21:15:55 +0300

    Add files via upload

---
+++ 2. Кролик.cpp
@@ -0,0 +1,25 @@
+﻿#include <iostream>
+int jumps(int stairs, int max_jump = 3) {
+    if (stairs == 0) {
+        return 1;
+    }
+    else if (stairs < 0) {
+        return 0;
+    }
+    int totalWays = 0;
+    for (int i = 1; i <= max_jump; ++i) {
+        totalWays += jumps(stairs - i, max_jump);
+    }
+    return totalWays;
+}
+
+int main()
+{
+    system("chcp 1251"); // Установить кодировку
+    int max_jump = 3;
+    int stairs;
+    std::cout << "максимальная длина прыжка: "
+        << max_jump << "\nколичество ступенек: ";
+    std::cin >> stairs;
+    std::cout << jumps(stairs, max_jump);
+}
