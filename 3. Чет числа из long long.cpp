a85340a3b7769775efdcbf78e53f573e7a4a46d0
ABissPro <109984434+ABissPro@users.noreply.github.com>
2025-02-26 23:24:55 +0300

    Add files via upload

---
+++ 3. Чет числа из long long.cpp
@@ -0,0 +1,20 @@
+﻿#include <iostream>
+
+void evendigits(long long n, int& ans) {
+    if (n == 0) {
+        return;
+    }
+    int digit = n % 10;
+    if (digit % 2 == 0) {
+        ans++;
+    }
+    evendigits(n / 10, ans);
+}
+
+int main() {
+    long long number = 9223372036854775806LL;
+    int ans = 0;
+    evendigits(number, ans);
+    std::cout << "Количество четных цифр: " << ans << std::endl;
+    return 0;
+}
